0bd7e81494e9505d0c437e59e5c0681f1456ef8c
Adam Wishne <99109711+awishne@users.noreply.github.com>
2023-08-01 22:37:20 -0500

    Update game() function

--- script.js
+++ script.js
@@ -1,42 +1,76 @@
+// Create function called getComputerChoice that will return a random choice from an array of choices
+
 function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
 
-function playRound(playerSelection, computerSelection) {
-    const player = playerSelection.toLowerCase();
-    const computer = computerSelection.toLowerCase();
-    
+// Write function that plays single round. Needs two parameters called playerSelection and computerSelection
+
+
+function singleRound(playerSelection, computerSelection) {
+
+    //playerSelection needs to be case-insensitive
+    let player = playerSelection.toLowerCase();
+    let computer = computerSelection.toLowerCase();
+
     if (player === computer) {
-        return 'It\'s a tie!';
+        return 'Its a tie!'; 
     } else if (player === 'rock') {
         if (computer === 'paper') {
-            return 'You lose! Paper beats Rock';
-        } else (computer === 'scissors') 
-            return 'You win! Rock beats Scissors';
-        }
-      
-     else if (player === 'paper') {
-        if (computer === 'scissors') {
-            return 'You lose! Scissors beats Paper';
-        } else (computer === 'rock');
-            return 'You win! Paper beats Rock';
-        }
-
-     else if (player === 'scissors') {
-        if (computer === 'rock') {
-            return 'You lose! Rock beats scissors';
-        } else (computer === 'paper') ;
-            return 'You win! Scissors beats Paper';
-        }
-    else (player !== 'rock' || 'paper' || 'scissors');
-        return 'Invalid input';
-    }
+            return "You Lose! Paper beats Rock!";
+    }   else (computer === 'scissors');
+            return "You Win! Rock beats Scissors!";
         
-    
+    } else if (player === 'paper') {
+        if (computer === 'rock') {
+            return "You Win! Paper beats Rock!";
+    }   else (computer === 'Scissors');
+            return "You Lose! Scissors beats Paper!";
 
-const playerSelection = prompt('Rock, Paper or Scissors?');
-const computerSelection = getComputerChoice();
-console.log('Computer: ' + computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+    } else if (player === 'scissors') {
+        if (computer === 'rock') {
+            return "You Lose! Rock beats Scissors!";        
+    } else (computer === 'paper');
+            return "You Win! Scissors beats Paper!";
+
+    } else {
+        return 'Your input is invalid!'; }
+}
+
+
+// Write function game(), use the singleRound function inside of game function. 5 round game that keeps score and reports winner or loser
+function game() {
+    //store score for both player and computer
+    let computerScore = 0;
+    let playerScore = 0;
+
+    //set parameters for when player or computer scores a point, loop 5 rounds and keep score
+    while (playerScore < 5 && computerScore < 5) {
+        let playerSelection = prompt('Rock, Paper, or Scissors?');
+        let computerSelection = getComputerChoice();
+        console.log('Computer chose ' + computerSelection);
+        console.log('Human chose '+ playerSelection); 
+        console.log(singleRound(playerSelection, computerSelection));
+    
+    if (singleRound(playerSelection, computerSelection).includes('Win')) {
+        playerScore++;
+    } else if (singleRound(playerSelection, computerSelection).includes('Lose')) {
+        computerScore++;
+    } else (singleRound(playerSelection, computerSelection).includes('tie'));
+
+        console.log ('Computer score is ' + computerScore + ' and your score is ' + playerScore );
+}
+ 
+    //Show the result of the game after 5 rounds
+    if (computerScore > playerScore) {
+        return "You lost! The computer won " + computerScore + " to " + playerScore; 
+    } else if (computerScore < playerScore) {
+        return "You won! You won " + playerScore + " to " + computerScore;
+    } else (computerScore === playerScore);
+        return "You tied with the Computer!";
+
+}
+
+console.log(game());
